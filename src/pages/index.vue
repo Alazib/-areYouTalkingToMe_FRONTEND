fb04f435fef77bbc5d140d546b887eb8f4bf4a06
J.Alberto Azevedo <alazib7@gmail.com>
2023-04-02 11:57:57 +0200

    getting user media

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,14 +5,17 @@
         <q-btn text-color="black" @click="createRoom()">Create new room</q-btn>
         <div>ID ROOM: {{ id_room }}</div>
         <div>PASSWORD: {{ password }}</div>
+        <video style="width: 300px; height: 200px" id="myVideo" />
       </div>
-      <div b-2px b-black grow>
+
+      <div b-2px b-black grow flex-col>
         <q-btn text-color="black" @click="answerCall()">Answer call</q-btn>
         <q-input v-model="id_room_input" placeholder="Insert ID room"></q-input>
         <q-input
           v-model="password_input"
           placeholder="Insert password"
         ></q-input>
+        <video style="width: 300px; height: 200px" id="friendVideo" />
       </div>
     </div>
 
@@ -24,105 +27,94 @@
 
 <script setup>
 import axios from 'axios';
-import VuePeerJS from 'vue-peerjs';
-import Peer from 'peerjs';
-
+import { Peer } from 'peerjs';
 useTitle('Vital - Homepage');
-
-onMounted(() => {
-  myPeer.on('open', (id) => {
-    console.log('own id ' + id);
-    ownId = id;
-    navigator.mediaDevices
-      .getUserMedia({
-        video: true,
-        audio: true,
-      })
-      .then((stream) => {
-        myStream = stream;
-        addVideoStream(ownId, stream);
-      });
-  });
-
-  myPeer.on('call', (call) => {
-    //This function is not triggered
-    call.answer(stream);
-    console.log('answering');
-
-    call.on('stream', (userVideoStream) => {
-      addVideoStream(call.peer, userVideoStream);
-    });
-  });
-});
-
-const myPeer = new Peer({
-  host: '/',
-  port: '3001',
-});
 
 const id_room = ref('');
 const password = ref('');
 const id_room_input = ref('');
 const password_input = ref('');
 
-function getAllRooms() {
-  axios({
-    method: 'get',
-    url: 'http://localhost:3001/api/rooms',
-    headers: {
-      Authorization:
-        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDFkZDg0Y2EyZGE3MGRhOTViOWU1MDUiLCJyb2xlIjpbInVzZXIiXSwiaWF0IjoxNjc5ODIyNTIyLCJleHAiOjE2ODI0MTQ1MjJ9.eZmc466hOt83ZNtJRRt2KNuZ2EHHj6IlkWni4mtmf9A',
-    },
-  }).then((res) => {
-    id_room.value = res.data.data[0]._id;
-    password.value = res.data.data[0].password;
-  });
-}
+const peer = new Peer('');
+
+peer.on('open', (id) => {
+  console.log('My peer ID is: ' + id);
+});
+
+// function getAllRooms() {
+//   axios({
+//     method: 'get',
+//     url: 'http://localhost:3001/api/rooms',
+//     headers: {
+//       Authorization:
+//         'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDFkZDg0Y2EyZGE3MGRhOTViOWU1MDUiLCJyb2xlIjpbInVzZXIiXSwiaWF0IjoxNjc5ODIyNTIyLCJleHAiOjE2ODI0MTQ1MjJ9.eZmc466hOt83ZNtJRRt2KNuZ2EHHj6IlkWni4mtmf9A',
+//     },
+//   }).then((res) => {
+//     console.log(res.data);
+//   });
+// }
 
 function createRoom() {
-  axios({
-    method: 'post',
-    url: 'http://localhost:3001/api/rooms',
-    data: {
-      id_host: '000000',
-      password: 'password_prueba',
-    },
-    headers: {
-      Authorization:
-        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDFkZDg0Y2EyZGE3MGRhOTViOWU1MDUiLCJyb2xlIjpbInVzZXIiXSwiaWF0IjoxNjc5ODIyNTIyLCJleHAiOjE2ODI0MTQ1MjJ9.eZmc466hOt83ZNtJRRt2KNuZ2EHHj6IlkWni4mtmf9A',
-    },
-  }).then((res) => {
-    console.log(res.data);
-    id_room.value = res.data.data.id_room;
+  const getUserMedia = navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: true,
   });
+
+  getUserMedia.then((stream) => {
+    const myVideo = document.getElementById('myVideo');
+    myVideo.srcObject = stream;
+    myVideo.play();
+
+    // peer.on('call', (call) => {
+    //   call.answer(stream);
+    //   call.on('stream', (remoteStream) => {
+    //     const friendVideo = document.getElementById('friendVideo');
+    //     friendVideo.srcObject = remoteStream;
+    //     friendVideo.play();
+    //   });
+    // });
+  });
+
+  getUserMedia.catch((err) => console.log(err));
+
+  // axios({
+  //   method: 'post',
+  //   url: 'http://localhost:3001/api/rooms',
+  //   data: {
+  //     id_host: '000000',
+  //     password: 'password_prueba',
+  //   },
+  //   headers: {
+  //     Authorization:
+  //       'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDFkZDg0Y2EyZGE3MGRhOTViOWU1MDUiLCJyb2xlIjpbInVzZXIiXSwiaWF0IjoxNjc5ODIyNTIyLCJleHAiOjE2ODI0MTQ1MjJ9.eZmc466hOt83ZNtJRRt2KNuZ2EHHj6IlkWni4mtmf9A',
+  //   },
+  // }).then((res) => {
+  //   console.log(res.data);
+  //   id_room.value = res.data.data.id_room;
+  // });
 }
 
 function answerCall() {
-  if (
-    id_room.value === id_room_input.value &&
-    password.value === password_input.value
-  ) {
-    alert('CONECTANDO...');
-    console.log(id_room_input.value, id_room.value);
-  }
+  const getUserMedia = navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: true,
+  });
+
+  getUserMedia.then((stream) => {
+    const myVideo = document.getElementById('myVideo');
+    myVideo.srcObject = stream;
+    myVideo.play();
+
+    // const call = peer.call(id_room_input.value, stream);
+    // call.answer(stream); // Answer the call with an A/V stream.
+    // call.on('stream', function (remoteStream) {
+    //   // Show stream in some video/canvas element.
+    //   const friendVideo = document.getElementById('friendVideo');
+    //   friendVideo.srcObject = remoteStream;
+    //   friendVideo.play();
+    // });
+  });
+
+  getUserMedia.catch((err) => console.log(err));
 }
-
-// function connectToNewUser(userId, stream) {
-//   console.log('connecting to ' + userId);
-//   const call = myPeer.call(userId, stream);
-//   call.on('stream', (userVideoStream) => {
-//     console.log('calling');
-//     addVideoStream(userId, userVideoStream);
-//   });
-//   call.on('close', () => {
-//     let i = videoList.map((video) => video.id).indexOf(call.peer);
-//     videoList.splice(i, 1);
-//   });
-//   peers[userId] = call;
-// }
-
-// function addVideoStream(id, stream) {
-//   console.log('adding video to list ' + id);
-//   videoList.push({ stream, id });
-// }
 </script>
