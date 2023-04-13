3b40457c0ed116b1cc1786f73a337b47bcdeb447
J.Alberto Azevedo <alazib7@gmail.com>
2023-04-13 09:59:21 +0200

    Chat code done

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,9 +3,11 @@
     <div class="test window flex h-40vw">
       <div b-2px b-black text-black grow flex-col>
         <q-btn text-color="black" @click="connect()">Connect</q-btn>
-        <div>ID ROOM: {{ id_room }}</div>
-        <q-input v-model="id_remote_room"></q-input>
-        <q-input v-model="send" placeholder="Send message..."></q-input>
+        <div>ID ROOM: {{ mi_id }}</div>
+        <q-input v-model="id_remote"></q-input>
+        <q-input v-model="message" placeholder="Send message..."></q-input>
+        <q-btn text-color="black" @click="send()">Send</q-btn>
+
         <span>{{ message }}</span>
         <video style="width: 300px; height: 200px" id="myVideo" />
       </div>
@@ -18,43 +20,48 @@
 import { Peer } from 'peerjs';
 useTitle('Vital - Homepage');
 
-const id_room = ref('');
-const id_remote_room = ref('');
+const mi_id = ref('');
+const id_remote = ref('');
 const message = ref('');
-const send = ref('');
+let connections = {};
 
 //Creating the Peer Server connections
 const peer = new Peer();
 
 peer.on('open', (id) => {
   console.log('Conexión con PEERJS creada', id);
-  id_room.value = id;
+  mi_id.value = id;
 });
 
-// Handle  data connection
 peer.on('connection', (conn) => {
-  alert('Conexión desde remoto establecida!');
-
-  //Reciving from remote
-  conn.on('data', (data) => {
-    console.log('Recibido', data);
-  });
-  //Sending to remote
-  conn.on('open', () => {
-    conn.send('hello!');
-  });
+  manager(conn);
 });
 
-//Connecting peers
 function connect() {
-  const conn = peer.connect(id_remote_room.value);
-  //Sending a message to peerRemoto
+  const conn = peer.connect(id_remote.value);
+  connections[id_remote.value] = conn;
+  manager(conn);
+}
+
+function send() {
+  connections[id_remote.value].send(message.value);
+}
+
+function manager(conn) {
   conn.on('open', () => {
-    console.log('Conexión con REMOTO creada');
-    conn.send(send.value);
+    console.log(conn);
+    id_remote.value = conn.peer;
+    connections[id_remote.value] = conn;
+    console.log('Conexión desde local/remoto establecida!');
+  });
+  conn.on('data', (data) => {
+    console.log('Recibido: ', data);
   });
   conn.on('error', (err) => {
     console.log(err);
+  });
+  conn.on('close', () => {
+    console.log('Conexión CERRADA');
   });
 }
 </script>
