cfe10ae1ea7dc4cff898df6c35c6fcb11dbd574a
J.Alberto Azevedo <alazib7@gmail.com>
2023-05-19 19:40:12 +0200

    login view and basic logic done

--- src/pages/auth/index.vue
+++ src/pages/auth/index.vue
@@ -1,11 +1,76 @@
 <template>
-  <q-page> <div>PLATANO</div></q-page>
+  <q-page flex justify-center items-center>
+    <div style="max-width: 400px">
+      <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+        <q-input
+          filled
+          v-model="email"
+          label="EMAIL"
+          lazy-rules
+          :rules="[
+            (val) => (val && val.length > 0) || 'Please type a valid e-mail',
+          ]"
+        />
+
+        <q-input
+          filled
+          type="password"
+          v-model="password"
+          label="PASSWORD"
+          lazy-rules
+          :rules="[
+            (val) => (val && val.length > 0) || 'Please type your password ',
+          ]"
+        />
+
+        <div>
+          <q-btn label="Submit" type="submit" color="primary" />
+          <q-btn
+            label="Reset"
+            type="reset"
+            color="primary"
+            flat
+            class="q-ml-sm"
+          />
+        </div>
+      </q-form>
+    </div>
+  </q-page>
 </template>
 
 <script setup lang="ts">
-const route = useRoute();
+import { useQuasar } from 'quasar';
 
-console.log(route.meta);
+const email = ref<string | null>('');
+const password = ref<string | null>('');
+
+const q = useQuasar();
+const router = useRouter();
+
+function onSubmit() {
+  axios({
+    method: 'POST',
+    url: `${import.meta.env.VITE_API_AUTH}/login`,
+    data: { email: email.value, password: password.value },
+  }).then((res) => {
+    if (res.status === 200) {
+      q.notify({
+        color: 'green-4',
+        textColor: 'white',
+        icon: 'cloud_done',
+        message: 'Submitted',
+        position: 'center',
+      });
+
+      router.replace('/');
+    }
+  });
+}
+
+function onReset() {
+  email.value = null;
+  password.value = null;
+}
 </script>
 
 <route lang="yaml">
