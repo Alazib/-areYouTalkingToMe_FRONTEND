775cd33b56a080c4a220d160a8f0da05ab189995
J.Alberto Azevedo <alazib7@gmail.com>
2023-05-26 10:49:55 +0200

    initial route changed

--- src/pages/dashboard.vue
+++ src/pages/dashboard.vue
@@ -1,17 +1,43 @@
-<script setup>
-useTitle('Vital - Admin');
-</script>
-
 <template>
-  <q-page
-    class="flex flex-col flex-center space-y-30 bg-[#18161e] text-gray-50"
-  >
-    <div uppercase>Welcome to the Dashboard Page</div>
-    <Footer />
+  <q-page class="bg-grey-3 flex flex-col">
+    <div class="test window flex grow">
+      <div b-2px b-black text-black grow flex-col>
+        <q-btn text-color="black" @click="openChat()"
+          >Chat with {{ users.remoteUser.name }}</q-btn
+        >
+      </div>
+    </div>
   </q-page>
 </template>
 
-<route lang="yaml">
-meta:
-  layout: admin
-</route>
+<script setup>
+import { accessToChatRoom } from 'src/services/apiRoomsRequests';
+import { connectWithPeerJs } from 'src/services/peerJs';
+import users from 'src/services/users';
+import { useAuthStore } from 'src/stores/auth';
+
+const auth = useAuthStore();
+console.log(auth.name);
+
+useTitle('Are U Talking 2 Me?');
+
+onMounted(() => {
+  connectWithPeerJs(users.localUser.id);
+});
+
+const router = useRouter();
+
+async function openChat() {
+  const chatRoomData = {
+    password: 'contraseña',
+    id_guest: users.remoteUser.id,
+    participants: [users.localUser.id, users.remoteUser.id],
+  };
+
+  const room = await accessToChatRoom(chatRoomData);
+
+  const { _id } = room;
+
+  router.push(`/chat/${_id}`);
+}
+</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,43 +1,5 @@
-<template>
-  <q-page class="bg-grey-3 flex flex-col">
-    <div class="test window flex grow">
-      <div b-2px b-black text-black grow flex-col>
-        <q-btn text-color="black" @click="openChat()"
-          >Chat with {{ users.remoteUser.name }}</q-btn
-        >
-      </div>
-    </div>
-  </q-page>
-</template>
-
 <script setup>
-import { accessToChatRoom } from 'src/services/apiRoomsRequests';
-import { connectWithPeerJs } from 'src/services/peerJs';
-import users from 'src/services/users';
-import { useAuthStore } from 'src/stores/auth';
-
-const auth = useAuthStore();
-console.log(auth.name);
-
-useTitle('Are U Talking 2 Me?');
-
-onMounted(() => {
-  connectWithPeerJs(users.localUser.id);
-});
-
 const router = useRouter();
 
-async function openChat() {
-  const chatRoomData = {
-    password: 'contraseña',
-    id_guest: users.remoteUser.id,
-    participants: [users.localUser.id, users.remoteUser.id],
-  };
-
-  const room = await accessToChatRoom(chatRoomData);
-
-  const { _id } = room;
-
-  router.push(`/chat/${_id}`);
-}
+router.replace('/dashboard');
 </script>
