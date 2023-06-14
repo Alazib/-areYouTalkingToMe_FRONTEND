125c8eaabad1d17345af635c16cad7e425ee2a7d
J.Alberto Azevedo <alazib7@gmail.com>
2023-06-14 13:06:26 +0200

    WIP

--- src/util/httpErrorHandler.ts
+++ src/util/httpErrorHandler.ts
@@ -3,7 +3,7 @@ export default function httpErrorHandler(error: Record<string, unknown>) {
 
   if (response) {
     const data = response.data as Record<string, unknown>;
-    const errorMessage = data.error;
+    const errorMessage = data.error ?? data.errors;
 
     return errorMessage;
   } else {
